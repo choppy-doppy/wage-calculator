f5797a605b1bf71f6339da97d215f5dcbc85910b
Choppy <kaedongeddes@sd28.org>
2023-09-22 11:27:46 -0700

    need to fix things but initial code yippie

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    for ( ; ; ) {
+        cout << "enter your wage in hours, type 'exit' to exit" << endl;
+        string userWage;
+        cin >> userWage;
+
+        if (userWage == "exit")
+            exit(2);
+        try {
+            int intWage = stoi(userWage);
+            cout << "enter how many hours you worked" << endl;
+
+            int userHours;
+            cin >> userHours;
+
+            int userPaycheckCurrent = intWage * userHours;
+
+            cout << "you've made $" << userPaycheckCurrent << "!\n" << endl;
+        }
+        catch (std::invalid_argument&) {
+            cout << "INVALID INPUT" << endl;
+            return 0;
+        }
+    }
+}
+
+
