f0fe6d77aa65820e77103463597fdf89f33daff5
Choppy <kaedongeddes@sd28.org>
2023-09-22 13:02:42 -0700

    bluhhgadfjhdsafasjkljfajsldkjafsdsa;fjd;sa
    :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3

--- main.cpp
+++ main.cpp
@@ -3,29 +3,28 @@
 using namespace std;
 
 int main () {
-    for ( ; ; ) {
-        cout << "enter your wage in hours, type 'exit' to exit" << endl;
-        string userWage;
-        cin >> userWage;
+    cout << "enter your wage in hours, type 'exit' to exit" << endl;
+    string userWage;
+    cin >> userWage;
 
-        if (userWage == "exit")
-            exit(2);
-        try {
-            int intWage = stoi(userWage);
-            cout << "enter how many hours you worked" << endl;
+    if (userWage == "exit")
+        exit(2);
+    try {
+        int intWage = stoi(userWage);
+        cout << "enter how many hours you worked" << endl;
 
-            int userHours;
-            cin >> userHours;
+        int userHours;
+        cin >> userHours;
 
-            int userPaycheckCurrent = intWage * userHours;
+        int userPaycheckCurrent = intWage * userHours;
 
-            cout << "you've made $" << userPaycheckCurrent << "!\n" << endl;
-        }
-        catch (std::invalid_argument&) {
-            cout << "INVALID INPUT" << endl;
-            return 0;
-        }
+        cout << "you've made $" << userPaycheckCurrent << "!\n" << endl;
     }
+    catch (std::invalid_argument&) {
+        cout << "INVALID INPUT" << endl;
+        return 1;
+    }
+    system("pause");
 }
 
 
